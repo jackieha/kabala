c887c1734b7fd036e5dee27d9df0f40b7283bacd
shuaifeng.gao <shuaifeng.gao@qunar.com>
2018-10-01 16:58:49 +0800

    init

---
+++ leedCode/src/contest/Main.java
@@ -0,0 +1,13 @@
+package contest;
+
+/**
+ * @author shuaifeng.gao
+ * @since 2018-10-01 16:58
+ **/
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("h");
+    }
+
+}
